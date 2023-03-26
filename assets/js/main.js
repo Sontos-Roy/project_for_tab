aeadef760c36db4cbdecc36ca770e63535708e84
Sontos Sharma <sontossharma926@gmail.com>
2023-03-26 10:45:35 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,4 +23,27 @@ $(document).ready(function(){
     $('#right-side-arrow-close').click(function(){
         $('.right-side').css('right', '-280px');
     });
+    // Get the elements and store them in variables
+    var $incrementBtn = $('.increment');
+    var $decrementBtn = $('.decrement');
+    var $counter = $('.counter');
+
+    // Set the initial counter value to 0.00
+    var counterValue = 1.0;
+
+    // Add a click event listener to the increment button
+    $incrementBtn.click(function() {
+      counterValue += 1.0;
+      $counter.text(counterValue.toFixed(1));
+    });
+
+    // Add a click event listener to the decrement button
+    $decrementBtn.click(function() {
+      if(counterValue > 1 ){
+        counterValue -= 1.0;
+      }else{
+        alert('You can not decrease Product quantity less then 1 !!!!');
+      }
+      $counter.text(counterValue.toFixed(1));
+    });
 });
